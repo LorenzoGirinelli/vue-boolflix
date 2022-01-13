4c8e6f10f292c335101528fdacd5470b7f0b6ae8
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-01-13 16:00:42 +0100

    milestone 3 copertina del film

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="movie">
-        <div>Titolo: {{details.title}} </div>
-        <div>
-            Titolo originale: {{details.original_title}} 
-        </div>
-        <div>
-            Lingua: {{details.original_language}}
-            </div>
-        <div>
-            Voto: {{details.vote_average}}
-        </div>
-        <div v-if="details.original_language === 'it'  || details.original_language === 'en' || details.original_language === 'pt'"  class="flag">
-             <img :src= "require( '../assets/img/' + details.original_language + '.png')" >
+        <div class="container">
+            <ul>
+                <div v-if="details.poster_path !== null" class="poster">
+                    <img :src="'https://image.tmdb.org/t/p/w342' + details.poster_path" :alt="details.name">
+                </div>
+                <div>Titolo: {{details.title}} </div>
+                <div v-if="details.original_title">
+                    Titolo originale: {{details.original_title}} 
+                </div>
+                <div v-else>Titolo originale: {{details.original_name}}</div>
+                <div>
+                    Lingua: {{details.original_language}}
+                </div>
+                <div>
+                    Voto: {{details.vote_average}}
+                </div>
+                <div v-if="details.original_language === 'it'  || details.original_language === 'en' || details.original_language === 'pt'"  class="flag">
+                    <img :src= "require( '../assets/img/' + details.original_language + '.png')" >
+                </div>
+            </ul>
         </div>
     </div>
 </template>
@@ -30,8 +38,15 @@ export default {
 .movie {
     margin: 10px;
     border: 1px solid black;
+    width: calc( (100% / 10) - 10px);
+    text-align: center;
     img {
         width: 20px;
+    }
+    .poster{
+        img{
+            width: 220px;
+        }
     }
 }
 </style>
